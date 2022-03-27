1d5e936b45fb5ea896a7650df50f6649d21240b7
Leo <leo.battalora@gmail.com>
2022-03-27 03:12:02 -0400

    fragment orchestration

--- app/src/main/java/edu/temple/audiobookplayer/MainActivity.kt
+++ app/src/main/java/edu/temple/audiobookplayer/MainActivity.kt
@@ -3,25 +3,80 @@ package edu.temple.audiobookplayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.FragmentContainerView
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity(), BookListFragment.BookListFragment {
+    private var bookDetailsContainer: FragmentContainerView? = null
+    private lateinit var bookListFragment: BookListFragment
+    private lateinit var bookDetailsFragment: BookDetailsFragment
+    private lateinit var bookViewModel: BookViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager
-            .beginTransaction()
-            .add(R.id.containerBookList, BookListFragment.newInstance(BookList.generateBooks()))
-            .commit()
+        bookDetailsContainer = findViewById<FragmentContainerView>(R.id.containerBookDetails)
+        bookListFragment = BookListFragment.newInstance(BookList.generateBooks())
+        bookDetailsFragment = BookDetailsFragment()
+        bookViewModel = ViewModelProvider(this).get(BookViewModel::class.java)
+
+        val bookSelected = bookViewModel.getBook().value != null
+
+        // determine which fragments should be shown
+        if (savedInstanceState == null) { // first load
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.containerBookList, bookListFragment)
+                .commit()
+        } else {
+            if (bookDetailsContainer == null) { // portrait
+                if (bookSelected) {
+                    // show detail fragment
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.containerBookList, bookDetailsFragment)
+                        .commit()
+                } else {
+                    // show list fragment
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.containerBookList, bookListFragment)
+                        .commit()
+                }
+            } else { // landscape
+                // make sure book list shows in containerBookList
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.containerBookList, bookListFragment)
+                    .commit()
+            }
+        }
     }
 
     override fun bookSelected() {
-        if (findViewById<FragmentContainerView>(R.id.containerBookDetails) == null) {
+        if (bookDetailsContainer == null) { // portrait
             supportFragmentManager
                 .beginTransaction()
-                .replace(R.id.containerBookList, BookDetailsFragment())
-                .addToBackStack(null)
+                .replace(R.id.containerBookList, bookDetailsFragment)
                 .commit()
+        }
+    }
+
+    override fun onBackPressed() {
+        val bookSelected = bookViewModel.getBook().value != null
+
+        if (bookDetailsContainer == null) { // portrait
+            if (bookSelected) {
+                bookViewModel.clearBook()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.containerBookList, bookListFragment)
+                    .commit()
+            } else {
+                this.finish()
+            }
+        } else { // landscape
+            this.finish()
         }
     }
 }
