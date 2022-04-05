8ce937c33099cf0a22ffbbcb64da864ccd6930c4
Leo <leo.battalora@gmail.com>
2022-04-05 01:03:11 -0400

    handle search intent and get data from API

--- app/src/main/java/edu/temple/audiobookplayer/MainActivity.kt
+++ app/src/main/java/edu/temple/audiobookplayer/MainActivity.kt
@@ -1,9 +1,20 @@
 package edu.temple.audiobookplayer
 
+import android.app.SearchManager
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.fragment.app.FragmentContainerView
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.Dispatcher
+import org.json.JSONArray
+import org.json.JSONObject
+import java.net.URL
 
 class MainActivity : AppCompatActivity(), BookListFragment.BookListFragment {
     private var bookDetailsContainer: FragmentContainerView? = null
@@ -14,6 +25,21 @@ class MainActivity : AppCompatActivity(), BookListFragment.BookListFragment {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // get query from search intent
+        if (Intent.ACTION_SEARCH == intent.action) {
+            intent.getStringExtra(SearchManager.QUERY)?.also { query ->
+                lifecycleScope.launch(Dispatchers.Main) {
+                    search(query)
+                }
+            }
+        }
+
+        // trigger search dialog on button click
+        val searchButton = findViewById<Button>(R.id.search)
+        searchButton.setOnClickListener {
+            onSearchRequested()
+        }
 
         bookDetailsContainer = findViewById<FragmentContainerView>(R.id.containerBookDetails)
         bookListFragment = BookListFragment.newInstance(BookList())
@@ -78,5 +104,31 @@ class MainActivity : AppCompatActivity(), BookListFragment.BookListFragment {
         } else { // landscape
             this.finish()
         }
+    }
+
+    private suspend fun search(query: String) {
+        println("[ STATUS ] query received: $query")
+
+        var responseArray: JSONArray
+        withContext(Dispatchers.IO) {
+            responseArray = JSONArray(
+                URL("https://kamorris.com/lab/cis3515/search.php?term=$query").readText()
+            )
+        }
+        println("[ STATUS ] response received: $responseArray")
+
+        val books = BookList()
+        for (i in 0 until responseArray.length()) {
+            val book = responseArray.getJSONObject(i)
+            books.add(
+                Book(
+                    book.getString("title"),
+                    book.getString("author"),
+                    book.getInt("id"),
+                    book.getString("cover_url"),
+                )
+            )
+        }
+        println("[ STATUS ] books received: $books")
     }
 }
