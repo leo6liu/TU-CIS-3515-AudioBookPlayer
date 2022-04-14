80843358d7009e9074321870dc89b6d932457c39
Leo B <leo.battalora@gmail.com>
2022-04-14 15:27:06 -0400

    created controls

---
+++ app/src/main/java/edu/temple/audiobookplayer/ControlsFragment.kt
@@ -0,0 +1,21 @@
+package edu.temple.audiobookplayer
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.squareup.picasso.Picasso
+
+class ControlsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val layout = inflater.inflate(R.layout.fragment_controls, container, false)
+        return layout
+    }
+}
