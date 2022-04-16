bb28be0ec13b43ba965eb27ab201ab2bcd7a3a7c
Leo <leo.battalora@gmail.com>
2022-04-16 03:34:10 -0400

    play pause logic

--- app/src/main/java/edu/temple/audiobookplayer/ControlsFragment.kt
+++ app/src/main/java/edu/temple/audiobookplayer/ControlsFragment.kt
@@ -5,17 +5,55 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.SeekBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.squareup.picasso.Picasso
 
 class ControlsFragment : Fragment() {
+    private lateinit var nowPlaying: TextView
+    private lateinit var seekBar: SeekBar
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         val layout = inflater.inflate(R.layout.fragment_controls, container, false)
+
+        layout.findViewById<FloatingActionButton>(R.id.buttonPlay).setOnClickListener { (requireActivity() as ControlsFragment).onPlayClicked() }
+        layout.findViewById<FloatingActionButton>(R.id.buttonPause).setOnClickListener { (requireActivity() as ControlsFragment).onPauseClicked() }
+        layout.findViewById<FloatingActionButton>(R.id.buttonStop).setOnClickListener { (requireActivity() as ControlsFragment).onStopClicked() }
+        seekBar = layout.findViewById(R.id.seekBar)
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    (requireActivity() as ControlsFragment).onSeekBarChanged(progress)
+                }
+            }
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+        })
+        nowPlaying = layout.findViewById(R.id.nowPlaying)
+
         return layout
+    }
+
+    fun updateBook(book: Book) {
+        nowPlaying.text = book.title
+        seekBar.max = book.duration
+    }
+
+    fun updateProgress(progress: Int) {
+        val seekBar = view?.findViewById<SeekBar>(R.id.seekBar)
+        seekBar?.progress = progress
+    }
+
+    interface ControlsFragment {
+        fun onPlayClicked()
+        fun onPauseClicked()
+        fun onStopClicked()
+        fun onSeekBarChanged(progress: Int)
     }
 }
--- app/src/main/java/edu/temple/audiobookplayer/MainActivity.kt
+++ app/src/main/java/edu/temple/audiobookplayer/MainActivity.kt
@@ -1,26 +1,55 @@
 package edu.temple.audiobookplayer
 
 import android.app.SearchManager
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.IBinder
+import android.os.Looper
 import android.widget.Button
 import androidx.fragment.app.FragmentContainerView
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import edu.temple.audlibplayer.PlayerService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.json.JSONArray
 import java.net.URL
 
-class MainActivity : AppCompatActivity(), BookListFragment.BookListFragment {
+class MainActivity : AppCompatActivity(), BookListFragment.BookListFragment, ControlsFragment.ControlsFragment {
     private var bookDetailsContainer: FragmentContainerView? = null
     private lateinit var bookListFragment: BookListFragment
     private lateinit var bookDetailsFragment: BookDetailsFragment
     private lateinit var controlsFragment: ControlsFragment
     private lateinit var bookViewModel: BookViewModel
     private lateinit var bookListViewModel: BookListViewModel
+
+    var isConnected = false
+    lateinit var mediaControlBinder: PlayerService.MediaControlBinder
+    val progressHandler = Handler(Looper.getMainLooper()) {
+        if (isConnected) {
+            it.obj?.let {
+                val progress = (it as PlayerService.BookProgress).progress
+                controlsFragment.updateProgress(progress)
+            }
+        }
+        return@Handler true
+    }
+    val serviceConnection = object: ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            isConnected = true
+            mediaControlBinder = service as PlayerService.MediaControlBinder
+            mediaControlBinder.setProgressHandler(progressHandler)
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            isConnected = false
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,6 +69,8 @@ class MainActivity : AppCompatActivity(), BookListFragment.BookListFragment {
         bookViewModel = ViewModelProvider(this).get(BookViewModel::class.java)
         bookListViewModel = ViewModelProvider(this).get(BookListViewModel::class.java)
 
+        bindService(Intent(this, PlayerService::class.java), serviceConnection, BIND_AUTO_CREATE)
+
         // keep track if book is selected
         val bookSelected = bookViewModel.getBook().value != null
 
@@ -47,8 +78,8 @@ class MainActivity : AppCompatActivity(), BookListFragment.BookListFragment {
         if (savedInstanceState == null) { // first load
             supportFragmentManager
                 .beginTransaction()
-                .add(R.id.containerBookList, bookListFragment)
-                .add(R.id.containerControls, controlsFragment)
+                .replace(R.id.containerBookList, bookListFragment)
+                .replace(R.id.containerControls, controlsFragment)
                 .commit()
         } else {
             if (bookDetailsContainer == null) { // portrait
@@ -89,12 +120,46 @@ class MainActivity : AppCompatActivity(), BookListFragment.BookListFragment {
     }
 
     // BookListFragment method to display details of selected book
-    override fun bookSelected() {
+    override fun bookSelected(book: Book) {
+        // start playing selected book
+        if (isConnected) {
+            mediaControlBinder.play(book.id)
+            controlsFragment.updateBook(book)
+        }
+
         if (bookDetailsContainer == null) { // portrait
             supportFragmentManager
                 .beginTransaction()
                 .replace(R.id.containerBookList, bookDetailsFragment)
                 .commit()
+        }
+    }
+
+    // ControlsFragment method which triggers on Play clicked
+    override fun onPlayClicked() {
+        if (isConnected && !mediaControlBinder.isPlaying) {
+            mediaControlBinder.pause()
+        }
+    }
+
+    // ControlsFragment method which triggers on Pause clicked
+    override fun onPauseClicked() {
+        if (isConnected && mediaControlBinder.isPlaying) {
+            mediaControlBinder.pause()
+        }
+    }
+
+    // ControlsFragment method which triggers on Stop clicked
+    override fun onStopClicked() {
+        if (isConnected) {
+            mediaControlBinder.stop()
+        }
+    }
+
+    // ControlsFragment method which triggers on SeekBar changed
+    override fun onSeekBarChanged(progress: Int) {
+        if (isConnected) {
+            mediaControlBinder.seekTo(progress)
         }
     }
 
