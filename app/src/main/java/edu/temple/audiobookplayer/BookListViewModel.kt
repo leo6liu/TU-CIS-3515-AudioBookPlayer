12e999fcf05c5f262b61dcd82647d5a4a5d95c90
Leo <leo.battalora@gmail.com>
2022-04-25 01:03:17 -0400

    make setting list be callable from coroutine

--- app/src/main/java/edu/temple/audiobookplayer/BookListViewModel.kt
+++ app/src/main/java/edu/temple/audiobookplayer/BookListViewModel.kt
@@ -2,15 +2,16 @@ package edu.temple.audiobookplayer
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
-class BookListViewModel: ViewModel() {
+class BookListViewModel : ViewModel() {
     private val bookList: MutableLiveData<BookList> by lazy {
         MutableLiveData<BookList>()
     }
 
     fun setList(bookList: BookList) {
-        this.bookList.value = bookList
+        this.bookList.postValue(bookList)
     }
 
     fun getList(): LiveData<BookList> {
